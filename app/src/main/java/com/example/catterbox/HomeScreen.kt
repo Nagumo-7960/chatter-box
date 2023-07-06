605b791056580683590940e2e20528473be120f8
sketchbook_01@icloud.com <sketchbook_01@icloud.com>
2023-07-06 23:16:09 +0900

    ルーム名を表示する

--- app/src/main/java/com/example/catterbox/HomeScreen.kt
+++ app/src/main/java/com/example/catterbox/HomeScreen.kt
@@ -1,14 +1,13 @@
 package com.example.catterbox
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.rememberNavController
 import com.example.catterbox.ui.theme.CatterBoxTheme
 
@@ -18,8 +17,19 @@ fun HomeScreen(toChatRoom: () -> Unit) {
         Box(modifier = Modifier.fillMaxSize(),
             contentAlignment = Alignment.Center
         ) {
-            Button(onClick = { toChatRoom() }) {
-                Text(text = "入室する")
+            Row(
+                modifier = Modifier.fillMaxSize(),
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Column(modifier = Modifier.padding(10.dp)) {
+                    Text(text = "だれでもルーム")
+                }
+                Column(modifier = Modifier.padding(10.dp)) {
+                    Button(onClick = { toChatRoom() }) {
+                        Text(text = "入室する")
+                    }
+                }
             }
         }
     }

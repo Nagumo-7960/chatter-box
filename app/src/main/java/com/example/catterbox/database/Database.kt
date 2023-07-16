d6f4bbe2cea4d3887ed5ae07f9410b8f1d0b83e7
sketchbook_01@icloud.com <sketchbook_01@icloud.com>
2023-07-17 00:12:01 +0900

    メッセージデータベースを作成

--- app/src/main/java/com/example/catterbox/database/Database.kt
+++ app/src/main/java/com/example/catterbox/database/Database.kt
@@ -2,10 +2,20 @@ package com.example.catterbox.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.example.catterbox.database.dao.MessageDAO
 import com.example.catterbox.database.dao.UserDAO
+import com.example.catterbox.database.model.MessageEntity
 import com.example.catterbox.database.model.UserEntity
 
-@Database(entities = [UserEntity::class], version = 1, exportSchema = false)
-abstract class Database: RoomDatabase() {
+@Database(
+    entities = [
+        UserEntity::class,
+        MessageEntity::class
+               ],
+    version = 1,
+    exportSchema = false
+)
+abstract class Database : RoomDatabase() {
     abstract fun userDao(): UserDAO
+    abstract fun messageDao(): MessageDAO
 }
---
+++ app/src/main/java/com/example/catterbox/database/dao/MessageDAO.kt
@@ -0,0 +1,21 @@
+package com.example.catterbox.database.dao
+
+import androidx.room.*
+import com.example.catterbox.database.model.MessageEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface MessageDAO {
+    @Query("select * from MessageEntity order by id desc")
+    fun getAll(): Flow<List<MessageEntity>>
+
+    //UIスレッドで実行できないためsuspend関数にする
+    @Insert
+    suspend fun create(messageEntity : MessageEntity)
+
+    @Update
+    suspend fun update(messageEntity: MessageEntity)
+
+    @Delete
+    suspend fun delete(messageEntity: MessageEntity)
+}

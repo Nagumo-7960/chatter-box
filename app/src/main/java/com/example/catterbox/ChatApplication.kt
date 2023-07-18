18846155ceb50344021f23425b4d31eb7ac16e31
sketchbook_01@icloud.com <sketchbook_01@icloud.com>
2023-07-18 20:14:32 +0900

    Applicationクラスでビルド

--- app/src/main/java/com/example/catterbox/ChatApplication.kt
+++ app/src/main/java/com/example/catterbox/ChatApplication.kt
@@ -2,11 +2,21 @@ package com.example.catterbox
 
 
 import android.app.Application
+import androidx.room.Room
 import com.example.catterbox.database.ChatDatabase
 
 class ChatApplication : Application() {
 
     companion object {
         lateinit var chatDatabase: ChatDatabase
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        chatDatabase = Room.databaseBuilder(
+            applicationContext,
+            ChatDatabase::class.java,
+            "chat-database"
+        ).build()
     }
 }

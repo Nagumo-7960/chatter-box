b6757a9e575d759a462634c8e6839e15c969c2dc
sketchbook_01@icloud.com <sketchbook_01@icloud.com>
2023-10-12 22:50:56 +0900

    部屋画面のレイアウトを修正

--- app/src/main/java/com/example/catterbox/presentation/screen/chatroom/ChatRoomScreen.kt
+++ app/src/main/java/com/example/catterbox/presentation/screen/chatroom/ChatRoomScreen.kt
@@ -1,5 +1,7 @@
 package com.example.catterbox.presentation.screen.chatroom
 
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
@@ -15,11 +17,19 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.CornerRadius
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Color.Companion.LightGray
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -88,25 +98,11 @@ fun ChatRoomContent(
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(bottom = 56.dp),
-                verticalArrangement = Arrangement.spacedBy(8.dp),
+                verticalArrangement = Arrangement.spacedBy(0.dp),
                 state = scrollState
             ) {
                 items(allMessages) { message ->
-                    Row {
-                        Text(
-                            text = message.post_user_name,
-                            modifier = Modifier.padding(top = 8.dp, bottom = 8.dp, end = 8.dp)
-                        )
-                        Text(
-                            text = message.message_content,
-                            modifier = Modifier
-                                .background(
-                                    color = Color(0xFF9BFF9F),
-                                    shape = RoundedCornerShape(50)
-                                )
-                                .padding(8.dp)
-                        )
-                    }
+                    ChatMessageSection(userName = message.post_user_name, message = message.message_content)
                 }
             }
         }
@@ -130,6 +126,60 @@ fun ChatRoomContent(
             scrollState.scrollToItem(allMessages.size)
         }
 
+    }
+}
+
+@Composable
+fun ChatMessageSection(
+    userName: String,
+    message: String
+){
+    // ChatContentで実装していた名前とメッセージの表示を切り分ける
+    Row (
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 8.dp)
+    ){
+        Column (
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ){
+            Column(
+                modifier = Modifier
+                    .padding(8.dp)
+                    .background(Color.Cyan, RoundedCornerShape(20))
+                    .size(65.dp)
+            ){
+                Image(
+                    painter = painterResource(id = com.example.catterbox.R.drawable.ic_launcher_foreground),
+                    contentDescription = "icon"
+                )
+            }
+            Text(
+                text = userName,
+                modifier = Modifier
+                    .width(80.dp),
+                textAlign = TextAlign.Center
+            )
+        }
+
+        Spacer(modifier = Modifier.padding(8.dp))
+
+        Box {
+            Box(modifier = Modifier.offset(x = -12.dp, y = 8.dp)){
+                BalloonComponent()
+            }
+            Text(
+                text = message,
+                modifier = Modifier
+                    .padding(top = 8.dp)
+                    .background(
+                        color = Color(0xFF9BFF9F),
+                        shape = RoundedCornerShape(16.dp)
+                    )
+                    .padding(8.dp)
+
+            )
+        }
     }
 }
 
@@ -160,9 +210,67 @@ fun inputTextField(
     )
 }
 
+@Composable
+fun BalloonComponent(){
+    Box (){
+        Canvas (
+            modifier = Modifier.size(25.dp)
+        ){
+            // 三角形の描画
+            val path = Path()
+            path.moveTo(size.width.toFloat(), 0F)
+            path.lineTo(size.width.toFloat(), size.height.toFloat())
+            path.lineTo(0F, size.height.toFloat())
+
+            drawPath(
+                path = path,
+                brush = SolidColor(Color(0xFF9BFF9F))
+            )
+        }
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun PreviewChatRoomScreen() {
+    // MessageEntityのダミーデータのリストを作成
+    val messageList = listOf(
+        MessageEntity(
+            id = 0,
+            post_user_name = "猫",
+            message_content = "みんな、最近何かおもしろいことがあった？",
+            created_at = System.currentTimeMillis(),
+            room_id = 0
+        ),
+        MessageEntity(
+            id = 1,
+            post_user_name = "犬",
+            message_content = "実は、昨日、飼っているペットの猫がピアノを演奏しているような真似をしてたの！",
+            created_at = System.currentTimeMillis(),
+            room_id = 0
+        ),
+        MessageEntity(
+            id = 2,
+            post_user_name = "虎",
+            message_content = "それはすごいね、花子。でも、俺の犬はたいてい自分の尾を追いかけてるだけさ。",
+            created_at = System.currentTimeMillis(),
+            room_id = 0
+        ),
+        MessageEntity(
+            id = 3,
+            post_user_name = "ドラゴン",
+            message_content = "健太くん、それってちょっと変かも（笑）。私の鳥は毎朝アラームクロックの代わりに騒ぎたててるよ。",
+            created_at = System.currentTimeMillis(),
+            room_id = 0
+        ),
+        MessageEntity(
+            id = 4,
+            post_user_name = "猫",
+            message_content = "おはよう",
+            created_at = System.currentTimeMillis(),
+            room_id = 0
+        )
+    )
     val navController = rememberNavController()
     ChatApplication.chatDatabase = Room.databaseBuilder(
         LocalContext.current,
@@ -172,10 +280,43 @@ fun PreviewChatRoomScreen() {
     CatterBoxTheme {
         ChatRoomContent(
             toHome = { navController.navigate("home") },
-            allMessages = listOf(),
+            allMessages = messageList,
             text = "気づいたら猫になってたんだー",
             onValueChange = {},
             onImeAction = {}
         )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewChatMessageSection() {
+    CatterBoxTheme {
+        Column {
+            ChatMessageSection(
+                userName = "猫",
+                message = "おはよう"
+            )
+            ChatMessageSection(
+                userName = "犬",
+                message = "おはよう。今日あったことなんだけどさ、話していい？話すね？僕は朝起きたら犬になっていたんだ。しかも、僕の妹も犬になっていた。"
+            )
+            ChatMessageSection(
+                userName = "虎",
+                message = "おはよう"
+            )
+            ChatMessageSection(
+                userName = "ドラゴンウーマン牛川",
+                message = "おはよう"
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewBalloonComponent() {
+    CatterBoxTheme {
+        BalloonComponent()
     }
 }
